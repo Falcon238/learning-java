656254abf6bf2f9bcccb38c2645788327096caf3
Falcon238 <238thlbae@gmail.com>
2018-05-26 18:44:27 +0200

    poglavlje 2

---
+++ P2/Racun1.java
@@ -0,0 +1,10 @@
+public class Racun1 {
+    /*izracunaj ukupni racun za 2kg jbuka po 12KM
+    3kg krompira po 2KM i 50pf i 2kg bresaka po 14KM
+    */
+    public static void main(String[] args) {
+        System.out.print("Racun je: ");
+        System.out.print(2*12 + 3*2.50 + 2*14);
+        System.out.print("KM.");
+    }
+}
